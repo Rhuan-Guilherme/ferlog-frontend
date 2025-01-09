539f1898bcff01d331add475d8d934cf5d83a2fd
Rhuan Guilherme <rhuan23032004@gmail.com>
2025-01-09 11:39:49 -0300

    Adicionando usuários vindos do back-end

--- src/pages/UsersPage/userPage.tsx
+++ src/pages/UsersPage/userPage.tsx
@@ -2,8 +2,34 @@ import { Button, Dialog, DropdownMenu, Table } from '@radix-ui/themes';
 import { Pencil, Trash } from 'phosphor-react';
 import { DropDonwButtons, PageConteiner, SectionContainer } from './styles';
 import { RegisterModal } from './components/RegisterModal/registerModal';
+import { useEffect, useState } from 'react';
+import { api } from '../../lib/axios';
+
+interface UserProps {
+  id: string;
+  name: string;
+  email: string;
+  cargo: string;
+  role: string;
+}
 
 export function UsersPage() {
+  const [users, setUsers] = useState<UserProps[]>([]);
+
+  async function getUsers() {
+    const response = await api.get('/user/all', {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('@ferlog/token'),
+      },
+    });
+
+    setUsers(response.data.user);
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <PageConteiner>
       <SectionContainer>
@@ -27,37 +53,40 @@ export function UsersPage() {
         </Table.Header>
 
         <Table.Body>
-          <Table.Row>
-            <Table.RowHeaderCell>Danilo Sousa</Table.RowHeaderCell>
-            <Table.Cell>danilo@gmail.com</Table.Cell>
-            <Table.Cell>(61) 99999-9999</Table.Cell>
-            <Table.Cell>Motorista</Table.Cell>
-            <Table.Cell>
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Button variant="surface">
-                    Opções
-                    <DropdownMenu.TriggerIcon />
-                  </Button>
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Item asChild>
-                    <DropDonwButtons variant="normal">
-                      <Pencil size={15} />
-                      Editar
-                    </DropDonwButtons>
-                  </DropdownMenu.Item>
-                  <DropdownMenu.Separator />
-                  <DropdownMenu.Item asChild>
-                    <DropDonwButtons variant="delete">
-                      <Trash size={15} />
-                      Excluir
-                    </DropDonwButtons>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            </Table.Cell>
-          </Table.Row>
+          {users &&
+            users.map((user) => (
+              <Table.Row key={user.id}>
+                <Table.RowHeaderCell>{user.name}</Table.RowHeaderCell>
+                <Table.Cell>{user.email}</Table.Cell>
+                <Table.Cell>(61) 99999-9999</Table.Cell>
+                <Table.Cell>{user.cargo}</Table.Cell>
+                <Table.Cell>
+                  <DropdownMenu.Root>
+                    <DropdownMenu.Trigger>
+                      <Button variant="surface">
+                        Opções
+                        <DropdownMenu.TriggerIcon />
+                      </Button>
+                    </DropdownMenu.Trigger>
+                    <DropdownMenu.Content>
+                      <DropdownMenu.Item asChild>
+                        <DropDonwButtons variant="normal">
+                          <Pencil size={15} />
+                          Editar
+                        </DropDonwButtons>
+                      </DropdownMenu.Item>
+                      <DropdownMenu.Separator />
+                      <DropdownMenu.Item asChild>
+                        <DropDonwButtons variant="delete">
+                          <Trash size={15} />
+                          Excluir
+                        </DropDonwButtons>
+                      </DropdownMenu.Item>
+                    </DropdownMenu.Content>
+                  </DropdownMenu.Root>
+                </Table.Cell>
+              </Table.Row>
+            ))}
         </Table.Body>
       </Table.Root>
     </PageConteiner>
